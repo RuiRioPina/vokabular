c01cb9477e353a5f8e774412c2c987376e0f42eb
Rui Pina <1201568@isep.ipp.pt>
2026-03-02 09:57:35 +0000

    feat: add utils tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,1 +1,22 @@
-// Implemented in Task 3
+import { normalizeWord, generateId } from '../src/utils.js';
+
+test('normalizeWord trims and lowercases', () => {
+  expect(normalizeWord('  Hund  ')).toBe('hund');
+  expect(normalizeWord('LERNEN')).toBe('lernen');
+  expect(normalizeWord('fahren')).toBe('fahren');
+});
+
+test('normalizeWord handles empty string', () => {
+  expect(normalizeWord('')).toBe('');
+});
+
+test('generateId returns a non-empty string', () => {
+  const id = generateId();
+  expect(typeof id).toBe('string');
+  expect(id.length).toBeGreaterThan(0);
+});
+
+test('generateId returns unique ids', () => {
+  const ids = new Set(Array.from({ length: 100 }, () => generateId()));
+  expect(ids.size).toBe(100);
+});
